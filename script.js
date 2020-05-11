6392f4d50c29402655556a6eb03ca45c84ae8002
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-11 16:46:30 +0530

    Structure Set

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+Date     : 11-May-2020
+*/
+
+window.addEventListener("load",function()
+{
+    this.alert("Welcome, Please open JS Console!! ");
+});
